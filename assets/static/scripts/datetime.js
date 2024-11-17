466161f678412e202a2883cbe7fe6a2e98eae178
Evelyn <k1lls0mebody@proton.me>
2024-11-17 07:18:23 -0500

    Update datetime.js

--- assets/static/scripts/datetime.js
+++ assets/static/scripts/datetime.js
@@ -1,9 +1,9 @@
 function updateDateTime() {
     let now = new Date();
     
-    let dateOptions = { timeZone: 'Europe/Berlin', day: 'numeric', month: 'numeric', year: 'numeric' };
+    let dateOptions = { timeZone: 'America/New_York', day: 'numeric', month: 'numeric', year: 'numeric' };
     document.getElementById('date').textContent = now.toLocaleDateString('de-de', dateOptions);
-    let timeOptions = { timeZone: 'Europe/Berlin', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    let timeOptions = { timeZone: 'America/New_York', hour: 'numeric', minute: 'numeric', second: 'numeric' };
     document.getElementById('time').textContent = now.toLocaleTimeString('de-de', timeOptions);
 }
 
